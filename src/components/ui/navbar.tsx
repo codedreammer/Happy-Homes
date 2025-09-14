01b3000f6b27e517162e50cb4f85c003257b9000
Akshay Mehta <nkanal38@gmail.com>
2025-09-14 10:25:21 +0530

    updated the colour theory

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -28,7 +28,7 @@ export function Navbar() {
               <Home className="h-5 w-5 text-primary-foreground" />
             </div>
             <span className="text-xl font-bold bg-gradient-primary bg-clip-text text-transparent">
-              DesifyHub
+              Happy Homes
             </span>
           </Link>
 
